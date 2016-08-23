8b887980c899f4d85ce63e077560b76c86e8e31c
TeSopheak <sopheak4kh@gmail.com>
2016-08-23 15:10:46 +0700

    Comments Testing
    I added two comment lines in MainActivity

--- CareerAndFutureMajor/app/src/main/java/com/example/sss/careerandfuturemajor/MainActivity.java
+++ CareerAndFutureMajor/app/src/main/java/com/example/sss/careerandfuturemajor/MainActivity.java
@@ -9,5 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+		
+		//Do some comments in the project
+		//Is testing pulling request :D
     }
 }
